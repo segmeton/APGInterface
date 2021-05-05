9b06faae423b7331b6767e164403cbea488670d3
Thanat Jumneanbun <46423509+ligoligo12@users.noreply.github.com>
2021-05-05 16:12:49 +0900

    login and register

--- src/app.js
+++ src/app.js
@@ -4,73 +4,102 @@
 
 
 //function runbotTwtich(){
-	const option = {
-		options: { debug: true },
-		connection: {
-			reconnect: true,
-			secure: true
-		},
-		identity: {
-			username: 'ligoligo12',
-			password: 'oauth:2adeuj6p5ui8ujtxmqwkf55sk5janf'
-		},
-		channels: [ 'ligoligo12' ]
+const option = {
+	options: { debug: true },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: 'ligoligo12',
+		password: 'oauth:2adeuj6p5ui8ujtxmqwkf55sk5janf'
+	},
+	channels: ['ligoligo12']
+}
+
+const client = new tmi.Client(option);
+
+client.connect();
+var twitch_username;
+let p1Click = false;
+let p2Click = false;
+
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if (self) return;
+
+	if (p1Click === true) {
+		client.say(channel, `@${tags.username}, p1+: Fighting`);
 	}
-	
-	const client = new tmi.Client(option);
-	
-	client.connect();
-	
-	let p1Click = false;
-	let p2Click = false;
-	client.on('message', (channel, tags, message, self) => {
-		// Ignore echoed messages.
-		if(self) return;
-		
-		if(p1Click === true)
-		{
-			client.say(channel, `@${tags.username}, p1+: Fighting`);
-		}
-		else if (p2Click === true)
-		{
-			client.say(channel, `@${tags.username}, p2-: Loser`);
-		}
-		//else if(message.toLowerCase() === '!hello') {
-			// "@alca, heya!"
+	else if (p2Click === true) {
+		client.say(channel, `@${tags.username}, p2-: Loser`);
+	}
+	//else if(message.toLowerCase() === '!hello') {
+	// "@alca, heya!"
 
-			//console.log(channel, tags, message, self);
-			
-			
-		//}
-	});
-
-    function sentMessageBotP1(){
-        var name = document.getElementById('input-text-twitch').value;
-        if(name === "")
-        {
-            alert("Please type your Twitch name");
-        }
-        else{
-            client.say(`#ligoligo12`, `@${name}, p1+: Fighting`);
-        }
-        
-    }
-	function sentMessageBotP2(){
-		var name = document.getElementById('input-text-twitch').value;
-
-		if(name === "")
-        {
-			alert("Please type your Twitch name");
-
-		}
-		else{
-			client.say(`#ligoligo12`, `@${name}, p2-: Loser`);
-		}
+	//console.log(channel, tags, message, self);
 
 
-    }
+	//}
+});
+
+function sentMessageBotP1cheer() {
+	var name = document.getElementById('input-text-twitch').value;
+	if (name === "") {
+		alert("Please type your Twitch name");
+	}
+	else {
+		client.say(`#ligoligo12`, `@${name}, p1+: Fighting`);
+		twitch_username = client.username;
+		console.log(client.getUsername());
+	}
+
+}
+function sentMessageBotP2jeer() {
+	var name = document.getElementById('input-text-twitch').value;
+
+	if (name === "") {
+		alert("Please type your Twitch name");
+
+	}
+	else {
+		client.say(`#ligoligo12`, `@${name}, p2-: Loser`);
+	}
+
+
+}
+function sentMessageBotP2cheer() {
+	var name = document.getElementById('input-text-twitch').value;
+	if (name === "") {
+		alert("Please type your Twitch name");
+	}
+	else {
+		client.say(`#ligoligo12`, `@${name}, p2+: Fighting`);
+		twitch_username = client.username;
+		console.log(client.getUsername());
+	}
+
+}
+function sentMessageBotP1jeer() {
+	var name = document.getElementById('input-text-twitch').value;
+
+	if (name === "") {
+		alert("Please type your Twitch name");
+
+	}
+	else {
+		client.say(`#ligoligo12`, `@${name}, p1-: Loser`);
+	}
+
+
+}
+
+
+
+
 //}
 
 
 
-		
+
